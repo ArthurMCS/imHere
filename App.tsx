b086195bde7bb8790d3d2e0c4f9d556371105186
Arthur Moreira <arthurmcs96@gmail.com>
2023-01-21 15:59:00 -0300

    config the status bar

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/screens/Home';
 
 export default function App() {
   return (
-    <Home />
+    <>
+      <Home />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+    </>
   );
 }
